0c5caedbbfa25ff0046bf5d8a718a253cdf0a635
Dimasik17 <dima.karlovych777@gmail.com>
2019-05-07 18:00:13 +0300

    Create Medicine.java

---
+++ src/medicine/Medicine.java
@@ -0,0 +1,12 @@
+package medicine;
+
+import java.awt.Color;
+
+public class Medicine {
+
+    public static void main(String[] args) {
+        Design d = new Design();
+        d.getContentPane().setBackground(Color.white);
+        d.setVisible(true);
+    }
+}
